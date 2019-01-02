60f833454eee48150877e8088fe93c2dea76d220
Cameron Smith <elitedjcam@gmail.com>
2019-01-02 10:05:17 -0500

    lookin pretty

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
-function calculate(){
-  let userInput;
+
+function drawRectangle() {
+  let height = 100
+  let width = 100
+  let xCoord = 250
+  let yCoord = 250
+
+  var rectAngle = document.getElementById('canvas2').getContext('2d');
+  rectAngle.strokeRect(height, width, xCoord, yCoord);
 }
