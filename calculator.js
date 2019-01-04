0e829515644799e5519410df19ce82542fa0f8cd
Cameron Smith <elitedjcam@gmail.com>
2019-01-04 10:48:10 -0500

    at least the buttons work

--- calculator.js
+++ calculator.js
@@ -1,5 +1,55 @@
 
 function display1() {
-  var div = document.getElementById("output")
-  div.innerHTML = "1"
+  var div = document.getElementById("output");
+  div.innerHTML = "1";
+}
+
+function display2() {
+  var div = document.getElementById("output");
+  div.innerHTML = "2";
+}
+
+function display3() {
+  var div = document.getElementById("output");
+  div.innerHTML = "3";
+}
+
+function display4() {
+  var div = document.getElementById("output");
+  div.innerHTML = "4";
+}
+
+function display5() {
+  var div = document.getElementById("output");
+  div.innerHTML = "5";
+}
+
+function display6() {
+    var div = document.getElementById("output");
+    div.innerHTML = "6";
+}
+
+function display7() {
+    var div = document.getElementById("output");
+    div.innerHTML = "7";
+}
+
+function display8() {
+    var div = document.getElementById("output");
+    div.innerHTML = "8";
+}
+
+function display9() {
+    var div = document.getElementById("output");
+    div.innerHTML = "9";
+}
+
+function display0() {
+    var div = document.getElementById("output");
+    div.innerHTML = "0";
+}
+
+function clear() {
+  var div = document.getElementById("output");
+  div.innerHTML = "&nbsp";
 }
