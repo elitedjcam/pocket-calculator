0e90f6750e496e502fd63938d67ab0c7bad1466f
Cameron Smith <elitedjcam@gmail.com>
2019-01-10 10:50:01 -0500

    msg

--- calculator.js
+++ calculator.js
@@ -1,62 +1,103 @@
 let str = "";
+var stat = 0;
 
 function display1() {
-  var div = document.getElementById("output");
-  str += "1"
-  div.innerHTML = str;
+  if (stat == 1) {
+    str = "1";
+  } else {
+    str += "1"
+}
+var div = document.getElementById("output");
+div.innerHTML = str;
 }
 
 function display2() {
+  if (stat == 1) {
+    str = "2";
+  } else {
+    str += "2"
+}
   var div = document.getElementById("output");
-  str += "2"
   div.innerHTML = str;
 }
 
 function display3() {
+  if (stat == 1) {
+    str = "3";
+  } else {
+    str += "3"
+}
   var div = document.getElementById("output");
-  str += "3"
   div.innerHTML = str;
 }
 
 function display4() {
+  if (stat == 1) {
+    str = "4";
+  } else {
+    str += "4"
+}
   var div = document.getElementById("output");
-  str += "4"
   div.innerHTML = str;
 }
 
 function display5() {
+  if (stat == 1) {
+    str = "5";
+  } else {
+    str += "5"
+}
   var div = document.getElementById("output");
-  str += "5"
   div.innerHTML = str;
 }
 
 function display6() {
+  if (stat == 1) {
+    str = "6";
+  } else {
+    str += "6"
+}
   var div = document.getElementById("output");
-  str += "6"
   div.innerHTML = str;
 }
 
 function display7() {
-    var div = document.getElementById("output");
+  if (stat == 1) {
+    str = "7";
+  } else {
     str += "7"
+}
+    var div = document.getElementById("output");
     div.innerHTML = str;
 }
 
 function display8() {
-    var div = document.getElementById("output");
+  if (stat == 1) {
+    str = "8";
+  } else {
     str += "8"
-    div.innerHTML = str;
+}
+  var div = document.getElementById("output");
+  div.innerHTML = str;
 }
 
 function display9() {
-    var div = document.getElementById("output");
-    str += "9"
-    div.innerHTML = str;
+  if (stat == 1) {
+    str = "9";
+  } else {
+    str += "9";
+}
+  var div = document.getElementById("output");
+  div.innerHTML = str;
 }
 
 function display0() {
-    var div = document.getElementById("output");
+  if (stat == 1) {
+    str = "0";
+  } else {
     str += "0"
+}
+    var div = document.getElementById("output");
     div.innerHTML = str;
 }
 
@@ -64,40 +105,61 @@ function ac() {
     var div = document.getElementById("output");
     div.innerHTML = 0;
     str = "";
+    stat = 0;
 }
 
 function displayAdd() {
   var div = document.getElementById("output");
   str += "+"
   div.innerHTML = str;
+  stat = 0;
 }
 
 function displaySubtract() {
   var div = document.getElementById("output");
   str += "-"
   div.innerHTML = str;
+  stat = 0;
 }
 
 function displayMult() {
   var div = document.getElementById("output");
   str += "*"
   div.innerHTML = str;
+  stat = 0;
 }
 
 function displayDivide() {
-
+  var div = document.getElementById("output");
+  str += "/"
+  div.innerHTML = str;
+  stat = 0;
 }
 
 function displayLeftParent() {
-
+  var div = document.getElementById("output");
+  str += "("
+  div.innerHTML = str;
+  stat = 0;
 }
 
 function displayRightParent() {
+  var div = document.getElementById("output");
+  str += ")"
+  div.innerHTML = str;
+  stat = 0;
+}
 
+function displayDecimal() {
+  var div = document.getElementById("output");
+  str += "."
+  div.innerHTML = str;
+  stat = 0;
 }
 
 function equalSign (){
   var div = document.getElementById("output");
   str = eval(str)
   div.innerHTML = str;
+  stat = 1;
 }
